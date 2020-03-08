61ab023789013ceee03cb8591c58c8bc285b2382
Chaim Lev Ari <chiptus@gmail.com>
2020-03-08 14:49:55 +0200

    feat(edge): add update deployment group page

--- app/edge/views/deployment-groups/edit/edit-deployment-group-controller.js
+++ app/edge/views/deployment-groups/edit/edit-deployment-group-controller.js
@@ -1,4 +1,51 @@
-angular.module('portainer.edge')
-.controller('EditDeploymentGroupController', [function EditDeploymentGroupController(){
-  
-}]);
+angular
+  .module('portainer.edge')
+  .controller(
+    'EditDeploymentGroupController',
+    function EditDeploymentGroupController(
+      DeploymentGroupService,
+      EndpointService,
+      GroupService,
+      TagService,
+      Notifications,
+      $state,
+      $scope
+    ) {
+      this.state = {
+        actionInProgress: false
+      };
+
+      this.$onInit = async function $onInit() {
+        const [
+          tags,
+          endpoints,
+          endpointGroups,
+          deploymentGroup
+        ] = await Promise.all([
+          TagService.tags(),
+          EndpointService.endpoints(),
+          GroupService.groups(),
+          DeploymentGroupService.deploymentGroup($state.params.deploymentGroupID)
+        ]);
+        this.tags = tags.map(t => t.Name);
+        this.endpoints = endpoints.value;
+        this.endpointGroups = endpointGroups;
+        this.model = deploymentGroup;
+        $scope.$digest();
+      };
+
+      this.updateGroup = updateGroup.bind(this);
+
+      async function updateGroup() {
+        this.state.actionInProgress = true;
+        try {
+          await DeploymentGroupService.update(this.model.Id, this.model);
+          Notifications.success('Registry successfully updated');
+          $state.go('edge.deploymentGroups');
+        } catch (err) {
+          Notifications.error('Failure', err, 'Unable to update registry');
+        }
+        this.state.actionInProgress = false;
+      }
+    }
+  );
