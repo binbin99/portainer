ebb4ecb0d7682cb20e372262747cf3524cea938d
Chaim Lev Ari <chiptus@gmail.com>
2020-03-16 18:15:42 +0200

    feat(edge): add basic edit page

--- app/edge/views/stacks/edit/edit-stack-controller.js
+++ app/edge/views/stacks/edit/edit-stack-controller.js
@@ -1,6 +1,16 @@
 import angular from 'angular';
 
-angular.module('portainer.edge')
-  .controller('EditStackController', function EditStackController(){
+angular
+  .module('portainer.edge')
+  .controller('EditStackController', function EditStackController(
+    EdgeStackService,
+    $state,
+    $scope
+  ) {
+    this.model = null;
 
+    this.$onInit = async function $onInit() {
+      this.model = await EdgeStackService.stack($state.params.id);
+      $scope.$digest();
+    };
   });
